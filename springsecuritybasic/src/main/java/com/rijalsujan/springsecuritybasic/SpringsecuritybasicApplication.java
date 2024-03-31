eaf107c56f7eb07586dd82f58944cf7af6b3f23d
rijalsujan09 <srijal@castera.io>
2024-03-30 20:20:49 -0400

    spring security wrapup

--- springsecuritybasic/src/main/java/com/rijalsujan/springsecuritybasic/SpringsecuritybasicApplication.java
+++ springsecuritybasic/src/main/java/com/rijalsujan/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -2,8 +2,10 @@ package com.rijalsujan.springsecuritybasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.rijalsujan.springsecuritybasic.controller")
 public class SpringsecuritybasicApplication {
 
 	public static void main(String[] args) {
--- springsecuritybasic/src/main/java/com/rijalsujan/springsecuritybasic/controller/WelcomeController.java
+++ springsecuritybasic/src/main/java/com/rijalsujan/springsecuritybasic/controller/WelcomeController.java
@@ -1,4 +1,15 @@
 package com.rijalsujan.springsecuritybasic.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
 public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "welcome to Spring Application with security";
+    }
 }
